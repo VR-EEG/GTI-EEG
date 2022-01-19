1f9cff713180b371dd38e34aa148bf8e3a9e560e
Linus <linus-tiemann@live.de>
2022-01-19 20:24:46 +0100

    implementing input for controllers

--- GTI_EEG_Experiment/Assets/Scripts/new Table Configuration/TableConfigurationManager.cs
+++ GTI_EEG_Experiment/Assets/Scripts/new Table Configuration/TableConfigurationManager.cs
@@ -21,15 +21,20 @@ public class TableConfigurationManager : MonoBehaviour
     public GameObject Table;
 
     public GameObject Room;
+
+    [SerializeField] private float Speed;
     
     private TableConfigurationController _tableConfigurationController;
     private TableCalibrationUI _tableCalibrationUI;
+    
 
     private float depth;
     private float length;
     private float height;
 
     private Vector2 horizontalMovement;
+
+    private bool _isActive;
 
     private void Awake()
     {
@@ -45,16 +50,51 @@ public class TableConfigurationManager : MonoBehaviour
         }
 
     }
-
     private void Update()
     {
-        throw new NotImplementedException();
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log("test");
+           SetActive(!_isActive);
+           _isActive = !_isActive;
+        }
     }
 
 
-    public void MovementHorizontalInput(SteamVR_Behaviour_Vector2 input, SteamVR_Input_Sources fromSource)
+    public void MoveLeft(SteamVR_Action_Boolean leftInput, SteamVR_Input_Sources fromSource)
     {
-        
+        Vector3 inputDirection = Vector3.left;
+        MoveTable(inputDirection);
+    }
+    
+    public void MoveRight(SteamVR_Action_Boolean rightInput, SteamVR_Input_Sources fromSource)
+    {
+        Vector3 inputDirection = Vector3.right;
+        MoveTable(inputDirection);
+    }
+    
+    public void MoveForward(SteamVR_Action_Boolean forwardInput, SteamVR_Input_Sources fromSource)
+    {
+        Vector3 inputDirection = Vector3.forward;
+        MoveTable(inputDirection);
+    }
+    
+    public void MoveBackward(SteamVR_Action_Boolean backwardInput, SteamVR_Input_Sources fromSource)
+    {
+        Vector3 inputDirection = Vector3.back;
+        MoveTable(inputDirection);
+    }
+    
+    public void MoveUp(SteamVR_Action_Boolean upInput, SteamVR_Input_Sources fromSource)
+    {
+        Vector3 inputDirection = Vector3.up;
+        MoveTable(inputDirection);
+    }
+    
+    public void MoveDown(SteamVR_Action_Boolean downInput, SteamVR_Input_Sources fromSource)
+    {
+        Vector3 inputDirection = Vector3.down;
+        MoveTable(inputDirection);
     }
 
     public void SetActive(bool state)
@@ -62,21 +102,25 @@ public class TableConfigurationManager : MonoBehaviour
         _tableCalibrationUI.SetActive(state);
         
         if (state)
+        {
             CalibrationActionSet.Activate();
+        }
         else
+        {
             CalibrationActionSet.Deactivate();
+        }
     }
     
-
-
-    public void MoveManuallyTable(Vector3 direction)
+    public void MoveTable(Vector3 direction)
     {
-        
+        Debug.Log("move " + direction);
+        Table.transform.position += direction*Speed*Time.deltaTime;
     }
-
+    
     // Start is called before the first frame update
     void Start()
     {
+        
         _tableCalibrationUI = GetComponent<TableCalibrationUI>();
         _tableConfigurationController = GetComponent<TableConfigurationController>();
         
@@ -85,7 +129,17 @@ public class TableConfigurationManager : MonoBehaviour
         Debug.Assert(Table!=null, "table was not assigned");
         Debug.Assert(Room!=null, "room is not assigned");
         _tableConfigurationController.Init(Table, Room);
+        
+        MoveLeftInput.AddOnStateDownListener(MoveLeft,SteamVR_Input_Sources.Any);
+        MoveRightInput.AddOnStateDownListener(MoveRight,SteamVR_Input_Sources.Any);
+        MoveFowardInput.AddOnStateDownListener(MoveForward,SteamVR_Input_Sources.Any);
+        MoveBackwardInput.AddOnStateDownListener(MoveBackward,SteamVR_Input_Sources.Any);
+        MoveUpwardInput.AddOnStateDownListener(MoveUp,SteamVR_Input_Sources.Any);
+        MoveDownWardInput.AddOnStateDownListener(MoveDown,SteamVR_Input_Sources.Any);
     }
+    
+    
+    
 
     public void AutoCalibrateTablePosition()
     {

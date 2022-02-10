293430291f23306fb627ae3f8537e71ada22b66b
Linus Tiemann <linus-tiemann@live.de>
2022-02-10 03:00:53 +0100

    incongruent grasping

--- GTI_EEG_Experiment/Assets/Scripts/new ToolGraspSystem/GripPointSelection.cs
+++ GTI_EEG_Experiment/Assets/Scripts/new ToolGraspSystem/GripPointSelection.cs
@@ -7,8 +7,6 @@ using Valve.VR.InteractionSystem;
 
 public class GripPointSelection : MonoBehaviour
 {
-    private int _currentToolOrientation;
-
     private Interactable _interactable;
 
     private SteamVR_Skeleton_Poser _skeletonPoser;
@@ -22,10 +20,17 @@ public class GripPointSelection : MonoBehaviour
 
     private bool _handleIsCloser;
 
-    private SteamVR_Skeleton_Poser.PoseBlendingBehaviour _handlePoseBlendingBehaviour;
-    private SteamVR_Skeleton_Poser.PoseBlendingBehaviour _effectorPoseBlendingBehaviour;
-
+    private SteamVR_Skeleton_Poser.PoseBlendingBehaviour _congruentHandlePoseBlendingBehaviour;
+    private SteamVR_Skeleton_Poser.PoseBlendingBehaviour _congruentEffectorPoseBlendingBehaviour;
+    
+    private SteamVR_Skeleton_Poser.PoseBlendingBehaviour _incongruentMainPoseBlendingBehavior;
+    private SteamVR_Skeleton_Poser.PoseBlendingBehaviour _incongruentHandlePoseBlendingBehaviour;
+    private SteamVR_Skeleton_Poser.PoseBlendingBehaviour _incongruentEffectorPoseBlendingBehaviour;
     private Hand _hand;
+
+    private bool _toolOrientationCongruent;
+    
+    
      
     // Start is called before the first frame update
     void Start()
@@ -33,16 +38,26 @@ public class GripPointSelection : MonoBehaviour
         _interactable = GetComponent<Interactable>();
         _skeletonPoser = GetComponent<SteamVR_Skeleton_Poser>();
 
-        _handlePoseBlendingBehaviour = _skeletonPoser.GetBlendingBehaviour("BlendToHandle");
-        _effectorPoseBlendingBehaviour = _skeletonPoser.GetBlendingBehaviour("BlendToEffector");
+        _congruentHandlePoseBlendingBehaviour = _skeletonPoser.GetBlendingBehaviour("BlendToHandle");
+        _congruentEffectorPoseBlendingBehaviour = _skeletonPoser.GetBlendingBehaviour("BlendToEffector");
+        
+        _incongruentMainPoseBlendingBehavior = _skeletonPoser.GetBlendingBehaviour("BlendToMainIncongruent");
+        _incongruentHandlePoseBlendingBehaviour = _skeletonPoser.GetBlendingBehaviour("BlendToHandleIncongruent");
+        _incongruentEffectorPoseBlendingBehaviour = _skeletonPoser.GetBlendingBehaviour("BlendToEffectorIncongruent");
+        
 
-        _interactable.onAttachedToHand += ObjectAttached; 
-        _interactable.onDetachedFromHand += ObjectDetached;
+        NewExperimentManager.Instance.OnToolShown += SetToolOrientation; 
+        
+
+        _interactable.onAttachedToHand += ObjectAttached;
+
 
         handleAttachmentTrigger.InsideAttachmentTrigger += HandCloseToHandle;
         //handleAttachmentTrigger.OutsideAttachmentTrigger += ;
         
        effectorAttachmentTrigger.InsideAttachmentTrigger += HandCloseToEffector;
+
+       
 
        _hand = Player.instance.rightHand;
     }
@@ -50,7 +65,72 @@ public class GripPointSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
+        if (_interactable.isHovering)
+        {
+
+
+            if (_toolOrientationCongruent)
+            {
+                _incongruentMainPoseBlendingBehavior.influence = 0;
+                _incongruentHandlePoseBlendingBehaviour.influence = 0;
+                _incongruentEffectorPoseBlendingBehaviour.influence = 0;
+                _congruentEffectorPoseBlendingBehaviour.influence = 1;
+                _congruentHandlePoseBlendingBehaviour.influence = 1;
+            }
+            else
+            {
+                _incongruentMainPoseBlendingBehavior.influence = 1;
+                _incongruentHandlePoseBlendingBehaviour.influence = 1;
+                _incongruentEffectorPoseBlendingBehaviour.influence = 1;
+                _congruentEffectorPoseBlendingBehaviour.influence = 0;
+                _congruentHandlePoseBlendingBehaviour.influence = 0;
+            }
+             
+            
+            
+            
+            if (_handleIsCloser)
+            {
+                var closestGraspPosition = CalculateClosestPointOnTool(this.transform.position,
+                    handleAttachmentTrigger.transform.position, _hand.hoverSphereTransform.position, true);
+            
+                var totalDistance = Vector3.Distance(this.transform.position, handleAttachmentTrigger.transform.position);
+                var distanceToGraspPoint = Vector3.Distance(this.transform.position, closestGraspPosition);
+                var placementRatio = Mathf.Clamp01(distanceToGraspPoint / totalDistance);
+                if (_toolOrientationCongruent)
+                {
+                    _congruentEffectorPoseBlendingBehaviour.value = 0f;
+                    _congruentHandlePoseBlendingBehaviour.value = placementRatio;
+                }
+                else
+                {
+                    _incongruentEffectorPoseBlendingBehaviour.value = 0f;
+                    _incongruentHandlePoseBlendingBehaviour.value = placementRatio;
+                }
+                
+            }
+            else
+            {
+                var closestGraspPosition = CalculateClosestPointOnTool(this.transform.position,
+                    effectorAttachmentTrigger.transform.position, _hand.hoverSphereTransform.position, false);
+                var totalDistance = Vector3.Distance(this.transform.position, effectorAttachmentTrigger.transform.position);
+                var distanceToGraspPoint = Vector3.Distance(this.transform.position, closestGraspPosition);
+                var placementRatio = Mathf.Clamp01(distanceToGraspPoint / totalDistance);
+               
+                if (_toolOrientationCongruent)
+                {
+                    _congruentHandlePoseBlendingBehaviour.value = 0f;
+                    _congruentEffectorPoseBlendingBehaviour.value = placementRatio;
+                }
+                else
+                {
+                    _incongruentHandlePoseBlendingBehaviour.value = 0f;
+                    _incongruentEffectorPoseBlendingBehaviour.value = placementRatio;
+                }
+            }
+        }
     }
 
 
@@ -59,31 +139,16 @@ public class GripPointSelection : MonoBehaviour
         
         
 
-        if (_handleIsCloser)
-        {
-            var closestGraspPosition = CalculateClosestPointOnTool(this.transform.position,
-                handleAttachmentTrigger.transform.position, _hand.transform.position, true);
-            
-            var totalDistance = Vector3.Distance(this.transform.position, handleAttachmentTrigger.transform.position);
-            var distanceToGraspPoint = Vector3.Distance(this.transform.position, closestGraspPosition);
-            var placementRatio = Mathf.Clamp01(distanceToGraspPoint / totalDistance);
-            _effectorPoseBlendingBehaviour.value = 0f;
-            _handlePoseBlendingBehaviour.value = placementRatio;
-        }
-        else
-        {
-            var closestGraspPosition = CalculateClosestPointOnTool(this.transform.position,
-                handleAttachmentTrigger.transform.position, _hand.transform.position, false);
-            var totalDistance = Vector3.Distance(this.transform.position, effectorAttachmentTrigger.transform.position);
-            var distanceToGraspPoint = Vector3.Distance(this.transform.position, closestGraspPosition);
-            var placementRatio = Mathf.Clamp01(distanceToGraspPoint / totalDistance);
-            _handlePoseBlendingBehaviour.value = 0f;
-            _effectorPoseBlendingBehaviour.value = placementRatio;
-        }
+       
         
         
     }
 
+
+    private void SetToolOrientation(object sender, ToolShownEventArgs toolShownEventArgs)
+    {
+        _toolOrientationCongruent = Convert.ToBoolean(toolShownEventArgs.orientation);
+    }
 
     private Vector3 CalculateClosestPointOnTool(Vector3 centerPosition, Vector3 edgePosition, Vector3 handPosition, bool centerIsLeft)
     {
@@ -117,9 +182,5 @@ public class GripPointSelection : MonoBehaviour
         _handleIsCloser = false;
     }
 
-    private void ObjectDetached(Hand hand)
-    {
-        _effectorPoseBlendingBehaviour.value = 0f;
-        _handlePoseBlendingBehaviour.value = 0f;
-    }
+    
 }

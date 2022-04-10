5845e4a25f3f7d14394c40436e67b56df1df2a49
Linus Tiemann <linus-tiemann@live.de>
2022-04-10 19:51:26 +0200

    added Data frame

---
+++ GTI_EEG_Experiment/Assets/Scripts/LSL/newLSLDataFrame.cs
@@ -0,0 +1,86 @@
+﻿namespace LSL
+{
+    public class LSLLiveDataFrame
+    {
+        //LIFE DATA  recorded every frame - all floats 
+        //HMD 
+        public float HMDPositionX;
+        public float HMDPositionY;
+        public float HMDPositionZ;
+
+        public float HMDRotationX;
+        public float HMDRotationY;
+        public float HMDRotationZ;
+
+        //controller
+        public float ControllerPositionX;
+        public float ControllerPositionY;
+        public float ControllerPositionZ;
+
+        public float ControllerRotationX;
+        public float ControllerRotationY;
+        public float ControllerRotationZ;
+        //Gaze Data
+        public float CombinedGazePositionX;
+        public float CombinedGazePositionY;
+        public float CombinedGazePositionZ;
+
+        public float CombinedGazeDirectionX;
+        public float CombinedGazeDirectionY;
+        public float CombinedGazeDirectionZ;
+        
+        //HitObjectInfo
+        public float IsObjectAttachedToHand; //1 is attached , 0 is not Attached
+        public float IsObjectHit; // 1 is hit , 0 is not hit 
+        public float HitPositionOnObjectX;
+        public float HitPositionOnObjectY;
+        public float HitPositionOnObjectZ;
+        
+        
+    }
+    
+    public class LSLTimeStampData
+    {
+        public double TimeStamp;
+    }
+    
+    
+    
+    public class LSLStaticDataFrame
+    {
+        //STATIC TRIAL DATA -for all trial information- all double
+        public float HMDPositionX;
+        public float HMDPositionY;
+        public float HMDPositionZ;
+
+        public float HMDRotationX;
+        public float HMDRotationY;
+        public float HMDRotationZ;
+
+        //controller
+        public float ControllerPositionX;
+        public float ControllerPositionY;
+        public float ControllerPositionZ;
+
+        public float ControllerRotationX;
+        public float ControllerRotationY;
+        public float ControllerRotationZ;
+        //Gaze Data
+        public float CombinedGazePositionX;
+        public float CombinedGazePositionY;
+        public float CombinedGazePositionZ;
+
+        public float CombinedGazeDirectionX;
+        public float CombinedGazeDirectionY;
+        public float CombinedGazeDirectionZ;
+        
+        //HitObjectInfo
+        public float IsObjectAttachedToHand; //1 is attached , 0 is not Attached
+        public float IsObjectHit; // 1 is hit , 0 is not hit 
+        public float HitPositionOnObjectX;
+        public float HitPositionOnObjectY;
+        public float HitPositionOnObjectZ;
+        
+        //STATIC TRIAL DATA -for all trial information- all double
+    }
+}

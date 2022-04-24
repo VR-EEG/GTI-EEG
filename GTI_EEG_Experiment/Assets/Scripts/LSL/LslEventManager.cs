8be8905751d6b3d4d7afb1ad287568abd0e6d725
Linus Tiemann <linus-tiemann@live.de>
2022-04-25 01:42:57 +0200

    added EventManager

--- GTI_EEG_Experiment/Assets/Scripts/LSL/LslEventManager.cs
+++ GTI_EEG_Experiment/Assets/Scripts/LSL/LslEventManager.cs
@@ -1,7 +1,80 @@
-﻿namespace LSL
-{
-    public class LSLEventManager
+﻿using System;
+using UnityEngine;
+
+
+    public class LslEventManager:MonoBehaviour
     {
+        private NewExperimentManager _experimentManager;
         
+        private int currentTrialId;
+        private int currentTrialTool;
+        private int currentTrialOrientation;
+        private int currentInstruction;
+
+        private void Start()
+        {
+            _experimentManager = NewExperimentManager.Instance;
+            
+            
+            _experimentManager.OnTrialBegin += CollectTrialBeginTimeStamp;
+            _experimentManager.OnTrialEnd += CollectTrialEndTimeStamp;
+
+            _experimentManager.OnBeepSound += CollectBeepSoundTimeStamp;
+            _experimentManager.OnCueShowBegin += CollectCueShowBeginTimeStamp;
+            _experimentManager.OnCueShowEnd += CollectCueShowEndTimeStamp;
+            _experimentManager.OnToolIsShown += CollectToolShownTimeStamp;
+            
+        }
+
+        
+        private void CollectTrialBeginTimeStamp(int trialNumber, int toolID, int task, int orientation)
+        {
+            double[] currentTimestamp =
+            {
+                TimeManager.Instance.GetCurrentUnixTimeStamp(),
+                trialNumber,
+                toolID,
+                task,
+                orientation
+            };
+            LSLStreams.Instance.lslOTrialStartMeasurementTimeStamp.push_sample(currentTimestamp);
+        }
+
+        private void CollectTrialEndTimeStamp()
+        {
+            double[] currentTimestamp = { TimeManager.Instance.GetCurrentUnixTimeStamp()};
+            LSLStreams.Instance.lslOButtonPressedTimeStamp.push_sample(currentTimestamp);
+        }
+
+        
+        private void CollectCueShowBeginTimeStamp()
+        {
+            double[] currentTimestamp = { TimeManager.Instance.GetCurrentUnixTimeStamp()};
+            LSLStreams.Instance.lslOCueTimeStamp.push_sample(currentTimestamp);
+        }
+        
+        private void CollectCueShowEndTimeStamp()
+        {
+            double[] currentTimestamp = { TimeManager.Instance.GetCurrentUnixTimeStamp()};
+            LSLStreams.Instance.lslOCueDisappearedTimeStamp.push_sample(currentTimestamp);
+        }
+
+        private void CollectBeepSoundTimeStamp()
+        {
+            double[] currentTimestamp = { TimeManager.Instance.GetCurrentUnixTimeStamp()};
+            LSLStreams.Instance.lslOBeepPlayedTimeStamp.push_sample(currentTimestamp);
+        }
+        
+        private void CollectToolShownTimeStamp()
+        {
+            double[] currentTimestamp = { TimeManager.Instance.GetCurrentUnixTimeStamp()};
+            LSLStreams.Instance.lslOObjectShownTimeStamp.push_sample(currentTimestamp);
+        }
+
+        private void GatherTrialInformation()
+        {
+            
+        }
+
+
     }
-}
